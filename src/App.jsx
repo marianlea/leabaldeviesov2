2c29bd62cc23e5d3bdc69bd5c7ed32040e1637f0
Lea Baldevieso <mlbaldevieso@gmail.com>
2025-05-26 13:32:31 +1000

    navbar modified

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,33 @@ import Contact from "./pages/contact";
 import SideBar from "./components/SideBar";
 
 const sideBarLinks = [
-  { path: "/", text: "home", element: <Home /> },
-  { path: "/about", text: "about", element: <About /> },
-  { path: "/projects", text: "projects", element: <Projects /> },
-  { path: "/contact", text: "contact", element: <Contact /> },
+  {
+    path: "/",
+    title: "home",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    title: "about",
+    element: <About />,
+  },
+  {
+    path: "/projects",
+    title: "projects",
+    element: <Projects />,
+  },
+  {
+    path: "/contact",
+    title: "contact",
+    element: <Contact />,
+  },
 ];
-
-const colours = [
-  "bg-ceramic-50",
-  "bg-cobalt-50",
-  "bg-cobalt-100",
-  "bg-eggyolk-100",
-];
-
-const customMargin = ["ml-[0rem]", "ml-[0.2rem]", "ml-[0.4rem]", "ml-[0.6rem]"];
 
 function App() {
   return (
     <BrowserRouter>
       <div className="layout-setter-container min-h-screen bg-ceramic-50 grid grid-cols-[85%_15%]">
-        <main className="py-20 px-6 relative">
+        <main className="py-20 pl-5 pr-7">
           <section className="font-main text-gray-300 text-5xl"></section>
           <Routes>
             {sideBarLinks.map((sideBarLink, idx) => (
@@ -38,33 +45,7 @@ function App() {
             ))}
           </Routes>
         </main>
-        {/* <SideBar
-          sideBarLinks={sideBarLinks}
-          colours={colours}
-          customMargin={customMargin}
-          className="h-full overflow-hidden"
-        /> */}
-        <aside className="h-full w-full flex flex-col">
-          <div className="home-button bg-ceramic-50 flex-1 flex justify-center items-center">
-            <div className="-rotate-90">home</div>
-          </div>
-          <div className="about-button bg-cobalt-100 flex-1 flex justify-center items-center rounded-tl-2xl">
-            <div className="-rotate-90">about</div>
-          </div>
-          <div className="projects-button bg-cobalt-100  h-full w-full flex-1 flex flex-row">
-            <div className="h-full w-1/12 bg-cobalt-100 flex justify-center items-center"></div>
-            <div className="bg-cobalt-50 w-11/12 flex items-center justify-center rounded-tl-2xl">
-              <div className="-rotate-90">projects</div>
-            </div>
-          </div>
-          <div className="about-button bg-cobalt-50 h-full w-full flex-1 flex flex-row">
-            <div className="h-full w-1/12 bg-cobalt-100"></div>
-            <div className="h-full w-1/12 bg-cobalt-50"></div>
-            <div className="bg-eggyolk-100 w-10/12 flex items-center justify-center rounded-tl-2xl">
-              <div className="-rotate-90">about</div>
-            </div>
-          </div>
-        </aside>
+        <SideBar className="h-full" />
       </div>
     </BrowserRouter>
   );
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,27 +1,52 @@
-import SideBarLinks from "./SideBarLinks";
-export default function SideBar({ sideBarLinks, colours, customMargin }) {
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+const colours = [
+  "bg-ceramic-50",
+  "bg-cobalt-50",
+  "bg-cobalt-100",
+  "bg-eggyolk-100",
+];
+export default function SideBar() {
+  const [activeTab, setActiveTab] = useState("home-tab");
   return (
-    <aside className="sidebar">
-      {/* <SideBarLinks
-        sideBarLinks={sideBarLinks}
-        colours={colours}
-        customMargin={customMargin}
-        className="sideBarLinks"
-      ></SideBarLinks>
-       */}
-
-      {/* <div className="bg-ceramic-50">
-        <div className="bg-cobalt-100">
-          <div className="bg-cobalt-50">
-            <div className="bg-eggyolk-100"></div>
-          </div>
+    <aside className="h-full flex flex-col fixed right-0 top-0 text-ceramic-50 ">
+      <NavLink
+        to="/"
+        className="home-tab tracking-widest home-button bg-ceramic-50 flex-1 flex justify-center items-center text-cobalt-50"
+      >
+        <div className="-rotate-90 ">home</div>
+      </NavLink>
+      <NavLink
+        to="/about"
+        className="about-tab flex-1 flex justify-center items-center rounded-tl-2xl tracking-widest"
+      >
+        <div className="h-full w-1/12 bg-ceramic-50"></div>
+        <div className="h-full w-11/12 flex justify-center items-center bg-cobalt-100 rounded-tl-2xl">
+          <div className="-rotate-90">about</div>
         </div>
-      </div> */}
-
-      <div className="bg-ceramic-50"></div>
-      <div className="bg-cobalt-100"></div>
-      <div className="bg-cobalt-50"></div>
-      <div className="bg-eggyolk-100"></div>
+      </NavLink>
+      <NavLink
+        to="/projects"
+        className="projects-tab bg-cobalt-100  h-full w-full flex-1 flex flex-row tracking-widest"
+      >
+        <div className="h-full w-1/12 bg-ceramic-50"></div>
+        <div className="h-full w-1/12 bg-cobalt-100 flex justify-center items-center"></div>
+        <div className="bg-cobalt-50 w-10/12 flex items-center justify-center rounded-tl-2xl">
+          <div className="-rotate-90">projects</div>
+        </div>
+      </NavLink>
+      <NavLink
+        to="/contact"
+        className="about-tab bg-cobalt-50 h-full w-full flex-1 flex flex-row tracking-widest"
+      >
+        <div className="h-full w-1/12 bg-ceramic-50"></div>
+        <div className="h-full w-1/12 bg-cobalt-100"></div>
+        <div className="h-full w-1/12 bg-cobalt-50"></div>
+        <div className="bg-eggyolk-100 w-9/12 flex items-center justify-center rounded-tl-2xl">
+          <div className="-rotate-90">contact</div>
+        </div>
+      </NavLink>
     </aside>
   );
 }
